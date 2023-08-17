73236e3ba86603ee303fc1f562e2d1d14dd1c6a0
jenniferp1793 <131495330+jenniferp1793@users.noreply.github.com>
2023-08-17 14:55:33 -0500

    Add files via upload

---
+++ controllers/bus.js
@@ -0,0 +1,87 @@
+import Bus from "../models/bus.js";
+
+const httpBus = {
+    getBuses: async (req, res) => {
+        try {
+            const buses = await Bus.find()
+            res.json({ buses })
+
+        } catch (error) {
+            res.status(400).json({ error })
+        }
+
+    },
+    getBus: async (req, res) => {
+        const {id}=req.params
+        try {
+            const bus = await Bus.findById(id)
+            res.json({ bus })
+            
+        } catch (error) {
+            res.status(400).json({error})
+        }
+    },
+
+    postBus: async (req, res) => {
+        try {
+            const { numero_bus,cantidad_asientos, empresa_asignada } = req.body
+            const bus = new Bus({ numero_bus, cantidad_asientos, empresa_asignada})
+            await bus.save()
+
+            res.json({ bus })
+        } catch (error) {
+            res.status(400).json({ error })
+        }
+
+    },
+    
+    putEditarBus: async (req,res) => {
+        try {
+            const {id}=req.params
+            const {cantidad_asientos, empresa_asignada}=req.body
+            const bus=await 
+                Bus.findByIdAndUpdate(id,{cantidad_asientos,empresa_asignada},{new:true});
+                res.json({ bus })
+        } catch (error) {
+            res.status(400).json({ error: "Error en el servidor" });
+        }
+    },
+    
+    deleteBus: async (req, res) => {
+        try {
+            const { numero_bus } = req.params;
+
+            const bus = await Bus.findOneAndDelete({ numero_bus });
+
+            if (!bus) {
+                return res.status(404).json({ error: "Bus no encontrado" });
+            }
+
+            res.json({ message: "Bus eliminado correctamente" });
+        } catch (error) {
+            res.status(400).json({ error: "Error en el servidor" });
+        }
+    },
+    putBusInactivar: async (req,res)=>{
+        try {
+            const {id}=req.params
+            const bus=await Bus.findByIdAndUpdate(id,{estado:0},{new:true})
+            res.json({bus})
+        } catch (error) {
+            res.status(400).json({error})
+            
+        }
+    },
+    putBusActivar: async (req,res)=>{
+        try {
+            const {id}=req.params
+            const bus=await Bus.findByIdAndUpdate(id,{estado:1},{new:true})
+            res.json({bus})
+        } catch (error) {
+            res.status(400).json({error})
+        }
+    }
+    
+}
+
+export default httpBus
---
+++ controllers/cliente.js
@@ -0,0 +1,92 @@
+import Cliente from "../models/cliente.js";
+
+const httpCliente = {
+
+    getCliente: async (req, res) => {
+        try {
+            const cliente = await Cliente.find()
+            res.json({ cliente })
+
+        } catch (error) {
+            res.status(400).json({ error })
+        }
+
+    },
+    getClienteId: async (req, res) => {
+        const { id } = req.params
+        try {
+            const cliente = await Cliente.findById({id})
+            res.json({ cliente })
+
+        } catch (error) {
+            res.status(400).json({ error })
+        }
+    },
+
+    postCliente: async (req, res) => {
+        try {
+            const { cedula, nombre, telefono } = req.body
+            const cliente = new Cliente({ cedula, nombre, telefono })
+            await cliente.save()
+
+            res.json({ cliente })
+        } catch (error) {
+            res.status(400).json({ error })
+        }
+
+
+    },
+    putCliente: async (req, res) => {
+        // try {
+        //     const {cedula}=req.params
+        //     const {nombre,telefono}=req.body
+        //     const cliente_editar = Cliente.find(cedula)
+        //     const cliente=await 
+        //     Cliente.findByIdAndUpdate(cliente_editar._id,{nombre,telefono},{new:true});
+        //     res.json({cliente})
+        // } catch (error) {
+        //     res.status(400).json({error})
+        // }
+        try {
+            const { id } = req.params
+            const { nombre, telefono } = req.body
+            const cliente = await
+                Cliente.findByIdAndUpdate(id, { nombre, telefono }, { new: true });
+            res.json({cliente})
+        } catch (error) {
+            res.status(400).json({error})
+        }
+
+    },
+    deleteCliente: async (req,res) => {
+        try {
+            const { id } = req.params
+            const cliente = await Cliente.findByIdAndDelete(id)
+            res.json(cliente + `Cliente eliminado`)
+        } catch (error) {
+            res.status(400).json({error})
+        }
+    },
+    putClienteInactivar: async (req,res)=>{
+        try {
+            const {id}=req.params
+            const cliente=await Cliente.findByIdAndUpdate(id,{estado:0},{new:true})
+            res.json({cliente})
+        } catch (error) {
+            res.status(400).json({error})
+            
+        }
+    },
+    putClienteActivar: async (req,res)=>{
+        try {
+            const {id}=req.params
+            const cliente=await Cliente.findByIdAndUpdate(id,{estado:1},{new:true})
+            res.json({cliente})
+        } catch (error) {
+            res.status(400).json({error})
+        }
+    }
+
+}
+
+export default httpCliente
