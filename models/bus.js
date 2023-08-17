0b667ea63574815280af45eab65bc9f5f3c5f1c5
jenniferp1793 <131495330+jenniferp1793@users.noreply.github.com>
2023-08-17 14:54:18 -0500

    Add files via upload

---
+++ models/bus.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const busSchema = new mongoose.Schema({
+    numero_bus: { type:Number, require:true, unique:true },
+    placa: {type:String, require:true,unique:true},
+    cantidad_asientos: { type: String, require:true  },
+    empresa_asignada: { type: String, require:true },
+    createAT : {type:Date,default: Date.now },
+    estado:{type:Boolean, default:1}
+
+});
+
+export default mongoose.model("Bus", busSchema)
---
+++ models/ticket.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+
+const ticketSchema = new mongoose.Schema({
+    vendedor_id: {type:mongoose.Schema.Types.ObjectId,ref:'Vendedor', require:true},
+    cliente_id:{type:mongoose.Schema.Types.ObjectId,ref:'Cliente', require:true},
+    ruta_id:{type:mongoose.Schema.Types.ObjectId,ref:'Ruta', require:true},
+    bus_id:{type:mongoose.Schema.Types.ObjectId,ref:'Bus', require:true},
+    fechahora_venta:{type:Date, require:true},
+    createAT : {type:Date,default: Date.now },
+    estado:{type:Boolean, default:1}
+})
+
+export default mongoose.model("Ticket", ticketSchema)
